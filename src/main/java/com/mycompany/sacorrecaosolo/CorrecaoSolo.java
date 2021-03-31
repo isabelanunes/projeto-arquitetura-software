602d1b65ba2747ec34829fcb812e21d471f5ae51
jvitorgf <59662112+jvitorgf@users.noreply.github.com>
2021-03-31 16:24:31 -0300

    Adicionado um método que devolve a qtd a aplicar no solo da tabela de correção de fósforo.

--- src/main/java/com/mycompany/sacorrecaosolo/CorrecaoSolo.java
+++ src/main/java/com/mycompany/sacorrecaosolo/CorrecaoSolo.java
@@ -3,29 +3,64 @@ package com.mycompany.sacorrecaosolo;
 
 public class CorrecaoSolo {
     
-    private double[] nutriente = new double [6];
+    private double[] nutrienteIdeal = new double [6];
     
     public double[] calcIdeal(int texturaSolo) {
         switch(texturaSolo){
             case 1:
-                nutriente[0] = 9;
-                nutriente[1] = 0.35;
-                nutriente[2] = 6;
-                nutriente[3] = 1.5;
-                nutriente[4] = 9;
-                nutriente[5] = 0;
-                return nutriente;
+                nutrienteIdeal[0] = 9;
+                nutrienteIdeal[1] = 0.35;
+                nutrienteIdeal[2] = 6;
+                nutrienteIdeal[3] = 1.5;
+                nutrienteIdeal[4] = 9;
+                nutrienteIdeal[5] = 0;
+                return nutrienteIdeal;
      
             case 2:
-                nutriente[0] = 12;
-                nutriente[1] = 0.25;
-                nutriente[2] = 4;
-                nutriente[3] = 1;
-                nutriente[4] = 6;
-                nutriente[5] = 0;                  
-                return nutriente;
+                nutrienteIdeal[0] = 12;
+                nutrienteIdeal[1] = 0.25;
+                nutrienteIdeal[2] = 4;
+                nutrienteIdeal[3] = 1;
+                nutrienteIdeal[4] = 6;
+                nutrienteIdeal[5] = 0;                  
+                return nutrienteIdeal;
         }
         
-        return nutriente;
+        return null;
+    }
+    
+    public double correcaoFosforo(double qtdSoloFosforo,int fonte,double teor, double eficiencia){
+        if(teor>qtdSoloFosforo){
+              
+        switch(fonte){
+            case 1:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/18)*100)/100;
+            case 2:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/41)*100)/100;
+            case 3:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/48)*100)/100;
+            case 4:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/45)*100)/100;
+            case 5:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/18)*100)/100;
+            case 6:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/33)*100)/100;
+            case 7:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/29)*100)/100;
+            case 8:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/32)*100)/100;
+            case 9:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/24)*100)/100;
+            case 10:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/18.5)*100)/100;
+            case 11:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/52)*100)/100;
+            case 12:
+                return (double) Math.round((((((teor - qtdSoloFosforo)*2)*2.29)*100/eficiencia)*100/18)*100)/100;
+             }
+        }
+        
+        return 0;
+
     }
 }
--- src/test/java/TesteCorrecaoSolo.java
+++ src/test/java/TesteCorrecaoSolo.java
@@ -2,7 +2,7 @@
 import com.mycompany.sacorrecaosolo.CorrecaoSolo;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-//
+
 public class TesteCorrecaoSolo {
     CorrecaoSolo ca = new CorrecaoSolo();
     
@@ -13,6 +13,18 @@ public class TesteCorrecaoSolo {
     @Test
     public void testaValoresIdeaisTextMedia(){
         assertArrayEquals(new double[]{12,0.25,4,1,6,0},ca.calcIdeal(2));
+    }
+    @Test
+    public void testaCorrecaoFosforoSuperfosfatoSimples(){
+        assertEquals(233,ca.correcaoFosforo(8.59, 1, 15, 70));
+    }
+    @Test
+    public void testaCorrecaoFosforoDAP(){
+        assertEquals(93.2,ca.correcaoFosforo(8.59, 4, 15, 70));
+    }
+    @Test
+    public void testaCorrecaoFosforoDaoui(){
+        assertEquals(131.06,ca.correcaoFosforo(8.59, 8, 15, 70));
     }
 }
     

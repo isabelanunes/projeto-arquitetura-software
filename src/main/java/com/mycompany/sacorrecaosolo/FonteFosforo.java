a7f802ea256d2b9e6f3587e650312c10061bfc6f
jvitorgf <59662112+jvitorgf@users.noreply.github.com>
2021-04-23 16:38:15 -0300

    Removendo estruturas condicionais da correção de fósforo

--- src/main/java/com/mycompany/sacorrecaosolo/FonteFosforo.java
+++ src/main/java/com/mycompany/sacorrecaosolo/FonteFosforo.java
@@ -1,17 +1,307 @@
-
 package com.mycompany.sacorrecaosolo;
 
-public enum FonteFosforo {
-    SUPERFOSFATO_SIMPLES,
-    SUPERFOSFATO_TRIPLO,
-    MAP,
-    DAP,
-    TERMOFOSFATO_YOORIN,
-    FOSFATO_ARAD,
-    FOSFATO_GAFSA,
-    FOSFATO_DAOUI,
-    FOSFATO_PATOS_DE_MINAS,
-    ESCORIA_DE_THOMAS,
-    ACIDO_FOSFORICO,
-    MULTIFOSFATO_MAGNESIANO
+public enum FonteFosforo implements CalculosCorrecaoFosforo {
+    SUPERFOSFATO_SIMPLES {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 18) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional1 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.1 * 10) / 10;
+            return Math.max(0, adicional1);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.28 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+
+    },
+    SUPERFOSFATO_TRIPLO {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 41) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.2 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    MAP {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 48) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.09 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    DAP {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 45) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.16 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    TERMOFOSFATO_YOORIN {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 18) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional1 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.15 * 10) / 10;
+            return Math.max(0, adicional1);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.28 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    FOSFATO_ARAD {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 33) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.52 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    FOSFATO_GAFSA {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 29) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.52 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    FOSFATO_DAOUI {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 32) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.45 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    FOSFATO_PATOS_DE_MINAS {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 24) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.28 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    ESCORIA_DE_THOMAS {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 18.5) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.44 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+
+    },
+    ACIDO_FOSFORICO {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 52) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+
+            return 0;
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    },
+    MULTIFOSFATO_MAGNESIANO {
+        @Override
+        public double calculaCorrecaoFosforoQtdAplicar(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double qtdAplicar = (double) Math.round((((((teor - qtdSoloFosforo) * 2) * 2.29) * 100 / eficiencia) * 100 / 18) * 100) / 100;
+            return Math.max(0, qtdAplicar);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional1(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional1 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.11 * 10) / 10;
+            return Math.max(0, adicional1);
+        }
+
+        @Override
+        public double calcFosforoCorrecaoAdicional2(double qtdSoloFosforo, int fonte, double teor, double eficiencia) {
+            double adicional2 = (double) Math.round(calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia) * 0.18 * 10) / 10;
+            return Math.max(0, adicional2);
+        }
+
+        @Override
+        public double calcCorrecaoFosforoCusto(double qtdSoloFosforo, int fonte, double teor, double eficiencia, double valorTon) {
+            double custo = (double) Math.round(valorTon * (calculaCorrecaoFosforoQtdAplicar(qtdSoloFosforo, fonte, teor, eficiencia)) / 1000 * 100) / 100;
+            return Math.max(0, custo);
+        }
+    }
 }
--- src/test/java/TesteCorrecaoSolo.java
+++ src/test/java/TesteCorrecaoSolo.java
@@ -22,119 +22,115 @@ public class TesteCorrecaoSolo {
 
     @Test
     public void testaCorrecaoFosforoSuperfosfatoSimples() {
-        assertEquals(233, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 1, 15, 70));
-        assertEquals("SUPERFOSFATO_SIMPLES",FonteFosforo.SUPERFOSFATO_SIMPLES.name());
-        assertEquals(23.3, ca.calcFosforoCorrecaoAdicional1(8.59, 1, 15, 70));
-        assertEquals(65.2, ca.calcFosforoCorrecaoAdicional2(8.59, 1, 15, 70));
+        assertEquals(233, FonteFosforo.SUPERFOSFATO_SIMPLES.calculaCorrecaoFosforoQtdAplicar(8.59, 1, 15, 70));
+        assertEquals("SUPERFOSFATO_SIMPLES", FonteFosforo.SUPERFOSFATO_SIMPLES.name());
+        assertEquals(23.3, FonteFosforo.SUPERFOSFATO_SIMPLES.calcFosforoCorrecaoAdicional1(8.59, 1, 15, 70));
+        assertEquals(65.2, FonteFosforo.SUPERFOSFATO_SIMPLES.calcFosforoCorrecaoAdicional2(8.59, 1, 15, 70));
         assertEquals("ENXOFRE", NutrientesAdicionais.ENXOFRE.name());
         assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
-        assertEquals(1.29, ca.calcCorrecaoFosforoCusto(8.59, 1, 15, 70, 5.53));
-
+        assertEquals(1.29, FonteFosforo.SUPERFOSFATO_SIMPLES.calcCorrecaoFosforoCusto(8.59, 1, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoSuperfosfatoTriplo() {
-        assertEquals(102.29, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 2, 15, 70));
-        assertEquals("SUPERFOSFATO_TRIPLO",FonteFosforo.SUPERFOSFATO_TRIPLO.name());
-        assertEquals(20.5, ca.calcFosforoCorrecaoAdicional2(8.59, 2, 15, 70));
+        assertEquals(102.29, FonteFosforo.SUPERFOSFATO_TRIPLO.calculaCorrecaoFosforoQtdAplicar(8.59, 2, 15, 70));
+        assertEquals("SUPERFOSFATO_TRIPLO", FonteFosforo.SUPERFOSFATO_TRIPLO.name());
+        assertEquals(20.5, FonteFosforo.SUPERFOSFATO_TRIPLO.calcFosforoCorrecaoAdicional2(8.59, 2, 15, 70));
         assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
-        assertEquals(0.57, ca.calcCorrecaoFosforoCusto(8.59, 2, 15, 70, 5.53));
+        assertEquals(0.57, FonteFosforo.SUPERFOSFATO_TRIPLO.calcCorrecaoFosforoCusto(8.59, 2, 15, 70, 5.53));
 
     }
 
     @Test
     public void testaCorrecaoFosforoMAP() {
-        assertEquals(87.37, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 3, 15, 70));
+        assertEquals(87.37, FonteFosforo.MAP.calculaCorrecaoFosforoQtdAplicar(8.59, 3, 15, 70));
         assertEquals("MAP", FonteFosforo.MAP.name());
-        assertEquals(7.9, ca.calcFosforoCorrecaoAdicional2(8.59, 3, 15, 70));
+        assertEquals(7.9, FonteFosforo.MAP.calcFosforoCorrecaoAdicional2(8.59, 3, 15, 70));
         assertEquals("NITROGENIO", NutrientesAdicionais.NITROGENIO.name());
-        assertEquals(0.48, ca.calcCorrecaoFosforoCusto(8.59, 3, 15, 70, 5.53));
-
+        assertEquals(0.48, FonteFosforo.MAP.calcCorrecaoFosforoCusto(8.59, 2, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoDAP() {
-        assertEquals(93.2, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 4, 15, 70));
+        assertEquals(93.2, FonteFosforo.DAP.calculaCorrecaoFosforoQtdAplicar(8.59, 4, 15, 70));
         assertEquals("DAP", FonteFosforo.DAP.name());
-        assertEquals(14.9, ca.calcFosforoCorrecaoAdicional2(8.59, 4, 15, 70));
+        assertEquals(14.9, FonteFosforo.DAP.calcFosforoCorrecaoAdicional2(8.59, 4, 15, 70));
         assertEquals("NITROGENIO", NutrientesAdicionais.NITROGENIO.name());
-        assertEquals(0.52, ca.calcCorrecaoFosforoCusto(8.59, 4, 15, 70, 5.53));
-
+        assertEquals(0.52, FonteFosforo.DAP.calcCorrecaoFosforoCusto(8.59, 4, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoYoorin() {
-        assertEquals(233, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 5, 15, 70));
+        assertEquals(233, FonteFosforo.TERMOFOSFATO_YOORIN.calculaCorrecaoFosforoQtdAplicar(8.59, 5, 15, 70));
         assertEquals("TERMOFOSFATO_YOORIN", FonteFosforo.TERMOFOSFATO_YOORIN.name());
-        assertEquals(34.9, ca.calcFosforoCorrecaoAdicional1(8.59, 5, 15, 70));
-        assertEquals(65.2, ca.calcFosforoCorrecaoAdicional2(8.59, 5, 15, 70));
-        assertEquals("MAGNESIO",NutrientesAdicionais.MAGNESIO.name());
+        assertEquals(34.9, FonteFosforo.TERMOFOSFATO_YOORIN.calcFosforoCorrecaoAdicional1(8.59, 5, 15, 70));
+        assertEquals(65.2, FonteFosforo.TERMOFOSFATO_YOORIN.calcFosforoCorrecaoAdicional2(8.59, 5, 15, 70));
+        assertEquals("MAGNESIO", NutrientesAdicionais.MAGNESIO.name());
         assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
-        assertEquals(1.29, ca.calcCorrecaoFosforoCusto(8.59, 5, 15, 70, 5.53));
-
+        assertEquals(1.29, FonteFosforo.TERMOFOSFATO_YOORIN.calcCorrecaoFosforoCusto(8.59, 5, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoFosfatoArad() {
-        assertEquals(127.09, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 6, 15, 70));
+        assertEquals(127.09, FonteFosforo.FOSFATO_ARAD.calculaCorrecaoFosforoQtdAplicar(8.59, 6, 15, 70));
         assertEquals("FOSFATO_ARAD", FonteFosforo.FOSFATO_ARAD.name());
-        assertEquals(66.1, ca.calcFosforoCorrecaoAdicional2(8.59, 6, 15, 70));
+        assertEquals(66.1, FonteFosforo.FOSFATO_ARAD.calcFosforoCorrecaoAdicional2(8.59, 6, 15, 70));
         assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
-        assertEquals(0.70, ca.calcCorrecaoFosforoCusto(8.59, 6, 15, 70, 5.53));
+        assertEquals(0.70, FonteFosforo.FOSFATO_ARAD.calcCorrecaoFosforoCusto(8.59, 6, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoReativoGafsa() {
-        assertEquals(144.62, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 7, 15, 70));
+        assertEquals(144.62, FonteFosforo.FOSFATO_GAFSA.calculaCorrecaoFosforoQtdAplicar(8.59, 7, 15, 70));
         assertEquals("FOSFATO_GAFSA", FonteFosforo.FOSFATO_GAFSA.name());
-        assertEquals(75.2, ca.calcFosforoCorrecaoAdicional2(8.59, 7, 15, 70));
-        assertEquals("CALCIO",NutrientesAdicionais.CALCIO.name());
-        assertEquals(0.80, ca.calcCorrecaoFosforoCusto(8.59, 7, 15, 70, 5.53));
+        assertEquals(75.2, FonteFosforo.FOSFATO_GAFSA.calcFosforoCorrecaoAdicional2(8.59, 7, 15, 70));
+        assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
+        assertEquals(0.80, FonteFosforo.FOSFATO_GAFSA.calcCorrecaoFosforoCusto(8.59, 7, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoDaoui() {
-        assertEquals(131.06, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 8, 15, 70));
+        assertEquals(131.06, FonteFosforo.FOSFATO_DAOUI.calculaCorrecaoFosforoQtdAplicar(8.59, 8, 15, 70));
         assertEquals("FOSFATO_DAOUI", FonteFosforo.FOSFATO_DAOUI.name());
-        assertEquals(59.0, ca.calcFosforoCorrecaoAdicional2(8.59, 8, 15, 70));
+        assertEquals(59.0, FonteFosforo.FOSFATO_DAOUI.calcFosforoCorrecaoAdicional2(8.59, 8, 15, 70));
         assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
-        assertEquals(0.72, ca.calcCorrecaoFosforoCusto(8.59, 8, 15, 70, 5.53));
+        assertEquals(0.72, FonteFosforo.FOSFATO_DAOUI.calcCorrecaoFosforoCusto(8.59, 8, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoPatosdeMinas() {
-        assertEquals(174.75, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 9, 15, 70));
+        assertEquals(174.75, FonteFosforo.FOSFATO_PATOS_DE_MINAS.calculaCorrecaoFosforoQtdAplicar(8.59, 9, 15, 70));
         assertEquals("FOSFATO_PATOS_DE_MINAS", FonteFosforo.FOSFATO_PATOS_DE_MINAS.name());
-        assertEquals(48.9, ca.calcFosforoCorrecaoAdicional2(8.59, 9, 15, 70));
+        assertEquals(48.9, FonteFosforo.FOSFATO_PATOS_DE_MINAS.calcFosforoCorrecaoAdicional2(8.59, 9, 15, 70));
         assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
-        assertEquals(0.97, ca.calcCorrecaoFosforoCusto(8.59, 9, 15, 70, 5.53));
+        assertEquals(0.97, FonteFosforo.FOSFATO_PATOS_DE_MINAS.calcCorrecaoFosforoCusto(8.59, 9, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoEscoriadeThomas() {
-        assertEquals(226.70, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 10, 15, 70));
+        assertEquals(226.70, FonteFosforo.ESCORIA_DE_THOMAS.calculaCorrecaoFosforoQtdAplicar(8.59, 10, 15, 70));
         assertEquals("ESCORIA_DE_THOMAS", FonteFosforo.ESCORIA_DE_THOMAS.name());
-        assertEquals(99.7, ca.calcFosforoCorrecaoAdicional2(8.59, 10, 15, 70));
+        assertEquals(99.7, FonteFosforo.ESCORIA_DE_THOMAS.calcFosforoCorrecaoAdicional2(8.59, 10, 15, 70));
         assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
-        assertEquals(1.25, ca.calcCorrecaoFosforoCusto(8.59, 10, 15, 70, 5.53));
+        assertEquals(1.25, FonteFosforo.ESCORIA_DE_THOMAS.calcCorrecaoFosforoCusto(8.59, 10, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoAcidoFosfórico() {
-        assertEquals(80.65, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 11, 15, 70));
-        assertEquals("ACIDO_FOSFORICO",FonteFosforo.ACIDO_FOSFORICO.name());
-        assertEquals(0.45, ca.calcCorrecaoFosforoCusto(8.59, 11, 15, 70, 5.53));
+        assertEquals(80.65, FonteFosforo.ACIDO_FOSFORICO.calculaCorrecaoFosforoQtdAplicar(8.59, 11, 15, 70));
+        assertEquals("ACIDO_FOSFORICO", FonteFosforo.ACIDO_FOSFORICO.name());
+        assertEquals(0.45, FonteFosforo.ACIDO_FOSFORICO.calcCorrecaoFosforoCusto(8.59, 11, 15, 70, 5.53));
     }
 
     @Test
     public void testaCorrecaoFosforoMultifosfatoMagnesiano() {
-        assertEquals(233, ca.calculaCorrecaoFosforoQtdAplicar(8.59, 12, 15, 70));
+        assertEquals(233, FonteFosforo.MULTIFOSFATO_MAGNESIANO.calculaCorrecaoFosforoQtdAplicar(8.59, 12, 15, 70));
         assertEquals("MULTIFOSFATO_MAGNESIANO", FonteFosforo.MULTIFOSFATO_MAGNESIANO.name());
-        assertEquals(25.6, ca.calcFosforoCorrecaoAdicional1(8.59, 12, 15, 70));
-        assertEquals(41.9, ca.calcFosforoCorrecaoAdicional2(8.59, 12, 15, 70));
+        assertEquals(25.6, FonteFosforo.MULTIFOSFATO_MAGNESIANO.calcFosforoCorrecaoAdicional1(8.59, 12, 15, 70));
+        assertEquals(41.9, FonteFosforo.MULTIFOSFATO_MAGNESIANO.calcFosforoCorrecaoAdicional2(8.59, 12, 15, 70));
         assertEquals("ENXOFRE", NutrientesAdicionais.ENXOFRE.name());
         assertEquals("CALCIO", NutrientesAdicionais.CALCIO.name());
-        assertEquals(1.29, ca.calcCorrecaoFosforoCusto(8.59, 12, 15, 70, 5.53));
+        assertEquals(1.29, FonteFosforo.MULTIFOSFATO_MAGNESIANO.calcCorrecaoFosforoCusto(8.59, 12, 15, 70, 5.53));
     }
 
     @Test
@@ -154,7 +150,7 @@ public class TesteCorrecaoSolo {
         assertEquals(3, ca.calcParticipacaoCTCSoloPotassioAposCorrecao(3));
         assertEquals(75.38, ca.calcCorrecaoPotassioCusto(0.15, 5.76, 1.63, 5.35, 2, 3, 150));
         assertEquals(85.43, ca.calcCorrecaoPotassioAdicional1(0.15, 5.76, 1.63, 5.35, 2, 3));
-        assertEquals("ENXOFRE",NutrientesAdicionais.ENXOFRE.name());
+        assertEquals("ENXOFRE", NutrientesAdicionais.ENXOFRE.name());
     }
 
     @Test
